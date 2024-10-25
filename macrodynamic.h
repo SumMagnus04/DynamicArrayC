d4abbab9aa8f904bc4ca65791e95a2aee1a928f5
SumMagnus04 <summagnus04@gmail.com>
2024-10-25 11:00:34 -0600

    Add files via upload

---
+++ macrodynamic.h
@@ -0,0 +1,50 @@
+#ifndef MACRODYNAMIC_H
+#define MACRODYNAMIC_H
+
+typedef struct DynamicArray
+{
+	void* data; // You must always case the data to the correct pointer type before use.
+	unsigned int size; // This is the actual number of elements.
+	unsigned int length; // This just keeps track of how much memory is in use.
+} DynamicArray;
+
+// Yes, these are macros. Welcome to generic typing in C
+#define dynamicInit(arr, first, type) \
+	do { \
+		arr.size = 1; \
+		arr.length = 10; \
+		arr.data = (type*)malloc(arr.length * sizeof(type)); \
+		if (arr.data == NULL) { \
+			printf("Fatal Error\n"); \
+			return; \
+		} \
+		((type*)arr.data)[arr.size - 1] = first; \
+	} while (0)
+
+#define dynamicAdd(arr, add, type) \
+	do { \
+		arr.size++; \
+		if (arr.size > arr.length) { \
+			arr.length *= 2; \
+			void* a = (type*)malloc(arr.length * sizeof(type)); \
+			if (a == NULL) { \
+				printf("Fatal Error\n"); \
+				return; \
+			} \
+			for (unsigned int i = 0; i < arr.size; i++) \
+				((type*)a)[i] = ((type*)arr.data)[i]; \
+			arr.data = a; \
+		} \
+		((type*)arr.data)[arr.size - 1] = add; \
+	} while (0)
+
+#define dynamicRemove(arr, index, type) \
+	do { \
+		if (index < 0 || index >= arr.size) return; \
+		arr.size--; \
+		if (index == arr.size) return; \
+		for (unsigned int i = index; i < arr.size; i++) \
+			((type*)arr.data)[i] = ((type*)arr.data)[i + 1]; \
+	} while (0)
+
+#endif
---
+++ main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "macrodynamic.h"
+
+typedef struct Vector
+{
+	float x;
+	float y;
+} Vector;
+
+int main()
+{
+	DynamicArray arr;
+	dynamicInit(arr, 10, int); // Always initialize the array before use.
+
+	printf("%d\n\n", ((int*)arr.data)[0]); // Note: the data is cast to an int* before use.
+
+	dynamicAdd(arr, 20, int);
+	for (unsigned int i = 0; i < arr.size; i++)
+		printf("%d\n", ((int*)arr.data)[i]);
+
+	printf("\n");
+	dynamicRemove(arr, 0, int);
+	printf("%d\n\n", ((int*)arr.data)[0]);
+
+	DynamicArray vecs;
+	Vector a;
+	a.x = 0.0f;
+	a.y = 0.0f;
+	dynamicInit(vecs, a, Vector); // Same as above but with a custom Vector struct.
+	Vector b;
+	b.x = 1.0f;
+	b.y = 1.0f;
+	dynamicAdd(vecs, b, Vector);
+	for (unsigned int i = 0; i < vecs.size; i++)
+		printf("x: %f, y: %f\n",
+			((Vector*)vecs.data)[i].x, // The data is still must be cast to Vector*.
+			((Vector*)vecs.data)[i].y
+		);
+	printf("\n");
+	dynamicRemove(vecs, 0, Vector);
+	for (unsigned int i = 0; i < vecs.size; i++)
+		printf("x: %f, y: %f\n",
+			((Vector*)vecs.data)[i].x,
+			((Vector*)vecs.data)[i].y
+		);
+
+	free(arr.data); // Remember to free the data.
+	free(vecs.data);
+
+	return 0;
+}
